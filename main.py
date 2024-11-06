48564f6a6de365a0f99b6f68dd73aef63fbff138
ekir999 <160941959+ekir999@users.noreply.github.com>
2024-11-06 01:03:39 -0600

    updated code displaying the game moves

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import cv2
 import gymnasium as gym
-
+import cv2
 from tetris_gymnasium.envs.tetris import Tetris
 
-env = gym.make("tetris_gymnasium/Tetris", render_mode="human")
-env.reset(seed=42)
+if __name__ == "__main__":
+    env = gym.make("tetris_gymnasium/Tetris", render_mode="ansi") # ansi --> uses terminal based graphics to display the game moves list, "human" --> uses a GUI interface to visualize the game
+    env.reset(seed=42)
 
-terminated = False
-while not terminated:
-    env.render()
-    action = env.action_space.sample()
-    observation, reward, terminated, truncated, info = env.step(action)
-    key = cv2.waitKey(150) # timeout to see the movement
-print("Game Over!")
+    terminated = False
+    while not terminated:
+        print(env.render() + "\n")
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+    cv2.waitKey(300) # timeout to see the movement
+    print("Game Over!")
